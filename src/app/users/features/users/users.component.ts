fbcef29cdca167e48df95ec91188bbf5f5139984
Aimurzin Ainur <aliveairush@gmail.com>
2023-06-18 20:04:48 +0300

    Forgot to auto save

--- src/app/users/features/users/users.component.ts
+++ src/app/users/features/users/users.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'ap   fp-users',
+  selector: 'app-users',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './users.component.html',
